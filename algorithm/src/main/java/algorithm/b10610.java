6d5a700e9c95547eb4c8a9464a26438da1312abd
WooGeun-Nam <-03@DESKTOP-88HGK66>
2022-12-23 16:03:49 +0900

    java.lang #finish

--- algorithm/src/main/java/algorithm/b10610.java
+++ algorithm/src/main/java/algorithm/b10610.java
@@ -1,10 +1,45 @@
 package algorithm;
 
+import java.util.Scanner;
+
 public class b10610 {
-
+	public static int drainCheck(String number) {
+		for(int i=0; i<number.length(); i++) {
+			if(number.charAt(i)=='0') {
+				return 1;
+			}
+		}
+		return -1;
+	}
+	
+	public static int sort(String number) {
+		char[] numberArray = number.toCharArray();
+		for(int i=0; i<number.length()-1; i++) {
+			int index = 0;
+			int max = number.charAt(i);
+			for(int j=i+1; j<number.length(); j++) {
+				if(max < number.charAt(j)) {
+					
+				}
+			}
+		}
+		return 0;
+	}
+	
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner( System.in );
+		int result;
 		
-
+		String number = Integer.toString(scanner.nextInt());
+		
+		result = drainCheck(number);
+		
+		if (result > 0) {
+			result = sort(number);
+		}
+		
+		System.out.println(result);
+		
 	}
 
 }
---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,23 @@
+package chapter04;
+
+public class StringTest01 {
+	public static void main(String[] args) {
+		// c:\temp
+		System.out.println("c:\\temp");
+		
+		// "HELLO"
+		System.out.println("\"HELLO\"");
+		
+		// \t : tap, \n : new line, \r : carriage return, \b : bell
+		
+		System.out.print("hello\tworld\n");
+		System.out.println("hello\twolrd");
+		
+		// '
+		char c = '\'';
+		System.out.println(c);
+		
+		System.out.println("1" + "\n");
+		System.out.println("2");
+	}
+}
