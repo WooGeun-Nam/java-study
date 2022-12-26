ff4704d67a6fd0f1a5e4a82d9a29ae54f87687d2
WooGeun-Nam <-03@DESKTOP-88HGK66>
2022-12-26 18:57:53 +0900

    prob3 수정

--- finaltest/src/main/java/prob03/Money.java
+++ finaltest/src/main/java/prob03/Money.java
@@ -5,29 +5,43 @@ public class Money {
 	// 해쉬코드 오버라이드
 	
 	/* 코드 작성 */
+	public Money() {
+	}
+	
 	public Money(int amount) {
 		this.amount = amount;
 	}
 
-	public int add(Money money) {
-		return amount+money.amount;
+	public Money add(Money money) {
+		Money m = new Money();
+		m.amount = amount+money.amount;
+		return m;
 	}
 
-	public int minus(Money money) {
-		return amount-money.amount;
+	public Money minus(Money money) {
+		Money m = new Money();
+		m.amount = amount-money.amount;
+		return m;
 	}
 
-	public int multiply(Money money) {
-		return amount*money.amount;
+	public Money multiply(Money money) {
+		Money m = new Money();
+		m.amount = amount*money.amount;
+		return m;
 	}
 
-	public int devide(Money money) {
-		return amount/money.amount;
+	public Money devide(Money money) {
+		Money m = new Money();
+		m.amount = amount/money.amount;
+		return m;
 	}
 	
 	@Override
 	public boolean equals(Object obj) {
-		return this.amount==(int)obj;
+		if(obj instanceof Money) {
+			return this.amount==((Money) obj).amount;
+		}
+		return false;
 	}
 	
 }
