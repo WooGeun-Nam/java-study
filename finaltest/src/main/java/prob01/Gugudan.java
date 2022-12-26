d91bbcb1ef136f6112838586a2eecfe2c68ab504
WooGeun-Nam <-03@DESKTOP-88HGK66>
2022-12-26 16:51:45 +0900

    finaltest #1

---
+++ finaltest/src/main/java/prob01/Gugudan.java
@@ -0,0 +1,7 @@
+package prob01;
+
+public class Gugudan {
+	private int lValue;
+	private int rValue;
+	//  해쉬셋
+}
---
+++ finaltest/src/main/java/prob03/Money.java
@@ -0,0 +1,7 @@
+package prob03;
+
+public class Money {
+	private int amount;
+	// 해쉬코드 오버라이드
+	/* 코드 작성 */
+}
