bceb5d627ee99d783e264ad0cf6138cf12993bfd
WooGeun-Nam <-03@DESKTOP-88HGK66>
2022-12-28 15:49:24 +0900

    NSLookup #finish

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -2,19 +2,24 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 	public static void main(String[] args) {
 		// 과제
-		String line = "www.douzone.com";
-		try {
-			InetAddress[] inetAddresses = InetAddress.getAllByName(line);
-			for(InetAddress Address : inetAddresses) {
-				System.out.println(Address);
-				// hostname : address
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			System.out.println("> ");
+			String line = scanner.nextLine();
+			try {
+				InetAddress[] inetAddresses = InetAddress.getAllByName(line);
+				for(InetAddress Address : inetAddresses) {
+					System.out.println(Address.getHostName()+" : "+Address.getHostAddress());
+					// hostname : address
+				}
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
 			}
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
 		}
 	}
 }
