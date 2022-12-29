80c7746b5dd5dddabbe5a515e7959fde410e0653
WooGeun-Nam <rolru7686@gmail.com>
2022-12-29 14:54:55 +0900

    httpd #2

--- network/src/main/java/httpd/RequestHandler.java
+++ network/src/main/java/httpd/RequestHandler.java
@@ -1,13 +1,16 @@
 package httpd;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
+	private static final String DOCUMENT_ROOT = "./webapp";
 	private Socket socket;
 
 	public RequestHandler(Socket socket) {
@@ -26,28 +29,47 @@ public class RequestHandler extends Thread {
 			InetSocketAddress inetSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
 			consoleLog("connected from " + inetSocketAddress.getAddress().getHostAddress() + ":"
 					+ inetSocketAddress.getPort());
-			
-			while(true) {
+
+			String request = null;
+			while (true) {
 				String line = br.readLine();
-				
+
 				// 브라우저가 연결을 끊으면
-				if(line == null) {
+				if (line == null) {
 					break; // 서버가 응답을 끊지않아서 크롬이 난리남 -> 무한루프
 				}
 				// SimpleHttpServer는 요청의 헤더만 처리한다.
-				if("".equals(line)) {
+				if ("".equals(line)) {
 					break;
 				}
-				
-				consoleLog(line);
+
+				// 요청 헤더의 첫 번째 라인만 읽음
+				if (request == null) {
+					request = line;
+					break;
+				}
 			}
-			
+
+			// 요청 처리
+			// consoleLog(request);
+			String[] tokens = request.split(" "); // 메소드, url, 프로토콜
+			if ("GET".equals(tokens[0])) { // CRUD, Create,Read,Update,Delete
+				consoleLog(request);
+				responseStaticResource(outputStream, tokens[1], tokens[2]);
+			} else {
+				// methods : POST, PUT, DELETE, HEAD, CONNECT
+				// SimpleHttpServer 에서는 무시(400 Bad Request).
+				// response가 없다면 문제가 생긴다.
+				// 과제
+				// response400Error(outputStream, tokens[2]);
+			}
+
 			// 예제 응답입니다.
 			// 서버 시작과 테스트를 마친 후, 주석 처리 합니다.
-			outputStream.write("HTTP/1.1 200 OK\r\n".getBytes("UTF-8"));
-			outputStream.write("Content-Type:text/html; charset=utf-8\r\n".getBytes("UTF-8"));
-			outputStream.write("\r\n".getBytes());
-			outputStream.write("<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes("UTF-8"));
+//			outputStream.write("HTTP/1.1 200 OK\r\n".getBytes("UTF-8"));
+//			outputStream.write("Content-Type:text/html; charset=utf-8\r\n".getBytes("UTF-8"));
+//			outputStream.write("\r\n".getBytes());
+//			outputStream.write("<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes("UTF-8"));
 
 		} catch (Exception ex) {
 			consoleLog("error:" + ex);
@@ -62,6 +84,23 @@ public class RequestHandler extends Thread {
 				consoleLog("error:" + ex);
 			}
 		}
+	}
+
+	private void responseStaticResource(OutputStream outputStream, String url, String protocol) throws IOException {
+		// default(welcome) file set
+		if ("/".equals(url)) {
+			url = "/index.html";
+		}
+		File file = new File(DOCUMENT_ROOT + url);
+		// nio
+		byte[] body = Files.readAllBytes(file.toPath());
+		String contentType = Files.probeContentType(file.toPath());
+		
+		// 응답
+		outputStream.write("HTTP/1.1 200 OK\r\n".getBytes("UTF-8"));
+		outputStream.write(("Content-Type:"+contentType+"; charset=utf-8\r\n").getBytes("UTF-8"));
+		outputStream.write("\r\n".getBytes());
+		outputStream.write(body);
 	}
 
 	public void consoleLog(String message) {
