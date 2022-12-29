d84e2996b1445f3627071379dfe8691913225c9b
WooGeun-Nam <rolru7686@gmail.com>
2022-12-29 13:55:45 +0900

    httpd #1

---
+++ network/src/main/java/httpd/RequestHandler.java
@@ -0,0 +1,70 @@
+package httpd;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class RequestHandler extends Thread {
+	private Socket socket;
+
+	public RequestHandler(Socket socket) {
+		this.socket = socket;
+	}
+
+	@Override
+	public void run() {
+		try {
+			// get IOStream
+			OutputStream outputStream = socket.getOutputStream();
+			// 자료, 이미지 등을 처리해야하기 때문에 byte처리가 편하다. (보조스트림 사용 X)
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			// logging Remote Host IP Address & Port
+			InetSocketAddress inetSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+			consoleLog("connected from " + inetSocketAddress.getAddress().getHostAddress() + ":"
+					+ inetSocketAddress.getPort());
+			
+			while(true) {
+				String line = br.readLine();
+				
+				// 브라우저가 연결을 끊으면
+				if(line == null) {
+					break; // 서버가 응답을 끊지않아서 크롬이 난리남 -> 무한루프
+				}
+				// SimpleHttpServer는 요청의 헤더만 처리한다.
+				if("".equals(line)) {
+					break;
+				}
+				
+				consoleLog(line);
+			}
+			
+			// 예제 응답입니다.
+			// 서버 시작과 테스트를 마친 후, 주석 처리 합니다.
+			outputStream.write("HTTP/1.1 200 OK\r\n".getBytes("UTF-8"));
+			outputStream.write("Content-Type:text/html; charset=utf-8\r\n".getBytes("UTF-8"));
+			outputStream.write("\r\n".getBytes());
+			outputStream.write("<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes("UTF-8"));
+
+		} catch (Exception ex) {
+			consoleLog("error:" + ex);
+		} finally {
+			// clean-up
+			try {
+				if (socket != null && socket.isClosed() == false) {
+					socket.close();
+				}
+
+			} catch (IOException ex) {
+				consoleLog("error:" + ex);
+			}
+		}
+	}
+
+	public void consoleLog(String message) {
+		System.out.println("[httpd#" + getId() + "] " + message);
+	}
+}
