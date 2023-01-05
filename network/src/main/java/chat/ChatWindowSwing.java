62a5db1c5ec3b756aa39e13bfd7e013b951960fc
WooGeun-Nam <rolru7686@gmail.com>
2023-01-05 20:59:14 +0900

    사용자 편의성 추가 - 리스트와 텍스트필드에서 전송, 도움말 따로 패널

--- network/src/main/java/chat/ChatWindowSwing.java
+++ network/src/main/java/chat/ChatWindowSwing.java
@@ -20,27 +20,52 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+
+import apple.laf.JRSUIConstants.Orientation;
 
 public class ChatWindowSwing {
 
 	private JFrame frame;
 	private JPanel pannel;
 	private JPanel pannel2;
+	private JPanel pannel3;
 	private JButton buttonSend;
 	private JTextField textField;
 	private JTextArea textArea;
 	private JList<String> userList;
 	private PrintWriter pw;
 	private BufferedReader br;
+    private JTextPane textPane;
 
 	public ChatWindowSwing(String name, PrintWriter pw, BufferedReader br) {
 		frame = new JFrame(name);
 		pannel = new JPanel();
 		pannel2 = new JPanel();
+		pannel3 = new JPanel();
 		buttonSend = new JButton("전송");
 		textField = new JTextField();
 		textArea = new JTextArea(30, 70);
 		userList = new JList<String>();
+		textPane = new JTextPane();
+		
+		textPane.setSize(15, 30);
+        StyledDocument doc = (StyledDocument) textPane.getDocument();
+        SimpleAttributeSet boldBlue = new SimpleAttributeSet();
+        StyleConstants.setFontFamily(boldBlue, "SansSerif");
+        StyleConstants.setFontSize(boldBlue, 14);
+        StyleConstants.setBold(boldBlue, true);
+        StyleConstants.setForeground(boldBlue, Color.BLACK);
+        try {
+			doc.insertString(doc.getLength(), "도움말은 \" /? , / \" 입니다.", boldBlue);
+		} catch (BadLocationException e) {
+			e.printStackTrace();
+		}
+		
 		this.pw = pw;
 		this.br = br;
 	}
@@ -89,20 +114,53 @@ public class ChatWindowSwing {
 		// TextArea
 		textArea.setEditable(false);
 		pannel2.add(BorderLayout.CENTER, textArea);
-		// userArea.setEditable(false);
 		pannel2.add(BorderLayout.EAST, userList);
 		frame.add(BorderLayout.CENTER, pannel2);
-
+		
+		// Noty
+		pannel3.add(textPane);
+		pannel3.setSize(15, 30);
+		frame.add(BorderLayout.NORTH, pannel3);
+		textPane.setEditable(false);
+		
 		// Frame
 		frame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				finish();
 			}
 		});
+		
+		userList.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				char keyCode = e.getKeyChar();
+				if (keyCode == KeyEvent.VK_ENTER) {
+					try {
+						sendMessage();
+					} catch (Exception e1) {
+						pw.println("ERROR#[UTF]" + e);
+					}
+				}
+			}
+		});
+		
+		textArea.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				char keyCode = e.getKeyChar();
+				if (keyCode == KeyEvent.VK_ENTER) {
+					try {
+						sendMessage();
+					} catch (Exception e1) {
+						pw.println("ERROR#[UTF]" + e);
+					}
+				}
+			}
+		});
+		
 		frame.setVisible(true);
 		frame.pack();
-
-		updateTextArea("접속을 환영합니다 도움말은 \"/?\" 입니다.");
+		
 		new ChatClientThread(br).start();
 	}
 
@@ -112,21 +170,30 @@ public class ChatWindowSwing {
 		System.exit(0);
 	}
 
+	private void helpDialog() {
+		JOptionPane.showMessageDialog(frame, "/c : 채팅기록 지우기, /q : 나가기\n"
+				+ "[리스트에서 대상선택후] /w : 귓속말 예) /w안녕 ");
+	}
+	
 	private void sendMessage() throws Exception {
 		String message = textField.getText();
 
+		if(message.equals("")) {
+			//공백이면 작동 x
+			return;
+		}
+		
 		// 명령어 인지 확인
 		String bash = message.substring(0, 1);
 		String cmd = null;
 		String msg = null;
 		if (message.equals("/")) {
-			JOptionPane.showMessageDialog(frame, "잘못된 명령어 입니다.\n");
+			helpDialog();
 		} else if (bash.equals("/")) {
 			cmd = message.substring(1, 2);
 			msg = message.substring(2);
 			if (cmd.equals("?")) {
-				JOptionPane.showMessageDialog(frame, "/c : 채팅기록 지우기, [리스트에서 대상선택후] /w : 귓속말, /q : 나가기\n");
-				// textArea.append("/c : 채팅기록 지우기, [리스트에서 대상선택후] /w : 귓속말, /q : 나가기\n");
+				helpDialog();
 			} else if (cmd.equals("c")) {
 				textArea.selectAll();
 				textArea.replaceSelection("");
@@ -143,7 +210,7 @@ public class ChatWindowSwing {
 			} else {
 				JOptionPane.showMessageDialog(frame, "잘못된 명령어 입니다.\n");
 			}
-		} else if (!message.equals("")) {
+		} else {
 			// 인코딩
 			String encodedString = Base64.getEncoder().encodeToString(message.getBytes("utf-8"));
 			// 프로토콜과 함께 담아서 전송
